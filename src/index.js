9fe481f33ccdd4c002649a5e1f6cec1404372dd3
salvadorf8 <aztecsalvador@gmail.com>
2020-07-31 12:12:38 -0500

    added gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <App increment={1} />
     </React.StrictMode>,
     document.getElementById('root')
 );
