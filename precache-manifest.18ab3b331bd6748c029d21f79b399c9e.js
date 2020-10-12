8144ee1b07e4847049694441c58bdf970b5d8ada
salvadorf8 <aztecsalvador@gmail.com>
2020-10-12 16:03:03 -0500

    Updates

---
+++ precache-manifest.18ab3b331bd6748c029d21f79b399c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8576522e67a9038cf70bcf16aabb4323",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b41926adcea67719ebe",
+    "url": "/monsters-rolodex/static/css/main.2b3e7d0c.chunk.css"
+  },
+  {
+    "revision": "6537d1c36a886b946142",
+    "url": "/monsters-rolodex/static/js/2.9e6a8291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9e6a8291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b41926adcea67719ebe",
+    "url": "/monsters-rolodex/static/js/main.35950f86.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
